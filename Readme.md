5d681a02b587f7f63d0ad0ea234e2ef8368612ea
Juanes Paz <juanes.pazj@gmail.com>
2026-04-25 10:26:36 +0100

    updated

---
+++ Readme.md
@@ -0,0 +1,2 @@
+find pins without guessing
+grep -n "UART7" target/thumbv7em-none-eabihf/release/build/embassy-stm32-*/out/_generated.rs
---
+++ src/main copy.rs
@@ -0,0 +1,74 @@
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::mode::Async;
+use embassy_stm32::peripherals::PG13;
+use embassy_stm32::usart::{Config, Uart, UartRx};
+use embassy_stm32::{bind_interrupts, dma, peripherals, usart};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::channel::Channel;
+use embassy_time::Timer;
+use {defmt_rtt as _, panic_probe as _};
+bind_interrupts!(struct Irqs {
+    UART7         => usart::InterruptHandler<peripherals::UART7>;
+    DMA1_STREAM0  => dma::InterruptHandler<peripherals::DMA1_CH0>;
+    DMA1_STREAM1  => dma::InterruptHandler<peripherals::DMA1_CH1>;
+});
+
+static CHANNEL: Channel<ThreadModeRawMutex, [u8; 8], 1> = Channel::new();
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) -> ! {
+    let p = embassy_stm32::init(Default::default());
+    info!("Hello World!");
+
+    let config = Config::default();
+    // let mut usart = Uart::new(
+    //     p.UART7, p.PA8,  // RX  ← valid RxPin<UART7> per compiler help
+    //     p.PA15, // TX  ← valid TxPin<UART7> per compiler help
+    //     p.DMA1_CH0, p.DMA1_CH1, // TX DMA
+    //     Irqs,       // RX DMA
+    //     config,
+    // )
+    // .unwrap();
+
+    let led = Output::new(p.PG13, Level::Low, Speed::Low);
+    spawner.spawn(blinky(led).unwrap());
+
+    //   unwrap!(usart.blocking_write(b"Type 8 chars to echo!\r\n"));
+
+    // let (mut tx, rx) = usart.split();
+    //   spawner.spawn(unwrap!(reader(rx)));
+    loop {
+        // let buf = CHANNEL.receive().await;
+        info!("writing...");
+        Timer::after_millis(100).await;
+        // unwrap!(tx.write(&buf).await);
+    }
+}
+
+#[embassy_executor::task]
+async fn reader(mut rx: UartRx<'static, Async>) {
+    let mut buf = [0; 8];
+    loop {
+        info!("reading...");
+        unwrap!(rx.read(&mut buf).await);
+        CHANNEL.send(buf).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn blinky(mut led: Output<'static>) -> ! {
+    loop {
+        info!("LED on");
+        led.set_high();
+        Timer::after_millis(500).await;
+
+        info!("LED off");
+        led.set_low();
+        Timer::after_millis(500).await;
+    }
+}
